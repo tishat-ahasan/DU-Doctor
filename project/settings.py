5067ed51ec61f8e0575c70174cdd407d919d6f9a
Mustafizur Rahman <mustafizur.rahman.1167141@gmail.com>
2021-02-26 21:13:45 +0600

    staticfiles folder added

--- project/settings.py
+++ project/settings.py
@@ -129,13 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static")
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
